d0f2f76c59d8cd7bde8c7c20aef67b01f5ea1736
s-blais <stephen@s-blais.com>
2020-11-19 10:18:21 -0500

    initial commit with some progress

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 console.log('%c HI', 'color: firebrick')
+
+document.addEventListener('DOMContentLoaded', function () {
+  loadImages();
+  // loadBreedOptions();
+});
+
+function loadImages() {
+  const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+  fetch(imgUrl)
+  .then(response => response.json())
+  //.then(json => console.log(json))
+  .then(json => addImages(json))
+}
+
+function addImages(json) {
+  const putThemHere = document.getElementById("dog-image-container")
+  // debugger
+  json.message.forEach(img => putThemHere.innerHTML += (`<img src='${img}'>`))
+}
